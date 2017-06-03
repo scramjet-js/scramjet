9602c6279ba20b47ce3a51e60170147c665aa9f8
Brock Peabody <brock.peabody@gmail.com>
2017-06-03 11:49:47 -0400

    rm sjtm_engine
    was just for getting started

--- src/apps/hello/main.cpp
+++ src/apps/hello/main.cpp
@@ -2,15 +2,10 @@
 
 #include <bslma_default.h>
 
-#include <sjtm_engine.h>
-
 #include <iostream>
 
 int main(int argc, char* argv[]) {
     using namespace BloombergLP;
-    bslma::Allocator *alloc = bslma::Default::defaultAllocator();
-    sjtm::Engine engine(alloc);
-    engine.setGlobal("foo", bdld::Datum::createNull());
     std::cout << "Hello world!\n";
     return 0;
 }
