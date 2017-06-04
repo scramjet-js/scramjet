41109225dcb4542543b0d4a296dbe33b0a40b333
Brock Peabody <brock.peabody@gmail.com>
2017-06-04 18:31:32 -0400

    Added 'evalbytecodes' program.
    Simple program to let you run a bytecode string.  Also rm'd test 'hello'
    program.

---
+++ src/apps/evalbytecode/main.cpp
@@ -0,0 +1,43 @@
+#include <sjtu_bytecodedslutil.h>
+#include <sjtu_interpretutil.h>
+
+#include <bdlma_sequentialallocator.h>
+
+#include <bsl_iostream.h>
+
+using namespace BloombergLP;
+
+namespace {
+void printUsage() {
+    bsl::cerr << "Usage:\n"
+        << "evalbytecode <bytecode DSL>\n\n"
+        << "Where <bytecode> is described in 'sjtu_bytedslutil.h', print the "
+        << "result.\n";
+}
+}
+
+int main(int argc, char* argv[]) {
+    if (2 != argc) {
+        printUsage();
+        return 1;
+    }
+    bdlma::SequentialAllocator alloc;
+    bsl::vector<sjtt::Bytecode> codes(&alloc);
+    bsl::string errorMessage;
+    sjtu::BytecodeDSLUtil::FunctionNameToAddressMap functions;
+    const int ret = sjtu::BytecodeDSLUtil::readDSL(&codes,
+                                                   &errorMessage,
+                                                   argv[1],
+                                                   functions);
+    if (0 != ret) {
+        bsl::cerr << errorMessage << "\n\n";
+        printUsage();
+        return 1;
+    }
+    const bdld::Datum value = sjtu::InterpretUtil::interpretBytecode(
+                                                                 &alloc,
+                                                                 &codes[0],
+                                                                 codes.size());
+    bsl::cout << value << '\n';
+    return 0;
+}
