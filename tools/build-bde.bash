06e500cefe4583beaafc95cb32fd422117e9ed30
Brock Peabody <brock.peabody@gmail.com>
2017-06-17 09:24:01 -0400

    Build bde in release.
    Most of what we get is through the headers anyway.  Also fixed bug in the
    script shebang.

--- tools/build-bde.bash
+++ tools/build-bde.bash
@@ -1,6 +1,6 @@
-#!/usr/bin/env python
+#!/usr/bin/env bash
 cd ..
 git submodule update --init .
 cd src/ext/bde
-../bde-tools/bin/waf configure
+../bde-tools/bin/waf configure --build-type release
 ../bde-tools/bin/waf build
