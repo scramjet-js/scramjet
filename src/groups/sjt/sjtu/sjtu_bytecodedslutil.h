ce681623696684984e9d2a1f9f356a74591b4057
Brock Peabody <brock.peabody@gmail.com>
2017-07-18 10:07:41 -0400

    check: redoing DSL

--- src/groups/sjt/sjtu/sjtu_bytecodedslutil.h
+++ src/groups/sjt/sjtu/sjtu_bytecodedslutil.h
@@ -15,8 +15,8 @@
 #include <sjtt_bytecode.h>
 #endif
 
-#ifndef INCLUDED_SJTD_DATUMUDTUTIL
-#include <sjtd_datumudtutil.h>
+#ifndef INCLUDED_SJTT_FUNCTION
+#include <sjtt_function.h>
 #endif
 
 namespace sjtu {
@@ -26,37 +26,51 @@ struct BytecodeDSLUtil {
     // 'sjtt::Bytecode' from and write 'sjtt::Bytecode' to the following DSL:
     //
     // DSL           = [<bytecode> ('|' <bytecode>)*]
-    // bytecode      = <push> |
-    //                 <load> |
+    // bytecode      = <allocate> |
+    //                 <allocateI32> |
+    //                 <allocateDbl> |
     //                 <store> |
+    //                 <storeI32> |
+    //                 <storeDbl> |
+    //                 <load> |
+    //                 <loadI32> |
+    //                 <loadDouble> |
+    //                 <eqI32> |
+    //                 <const> |
+    //                 <extractI32> |
+    //                 <extractDouble> |
     //                 <jump> |
-    //                 <if> |
-    //                 <if equal ints> |
-    //                 <equal ints> |
-    //                 <increment int> |
-    //                 <add doubles> |
+    //                 <ifI32> |
+    //                 <addI32> |
     //                 <call> |
-    //                 <execute> |
-    //                 <exit> |
-    //                 <resive>
-    // push          = 'P'<datum>
-    // load          = 'L'<int>
-    // store         = 'S'<int>
+    //                 <return>
+    // allocate      = 'A'<int>
+    // allocateI32   = 'Ai'<int>
+    // allocateDbl   = 'Ad'<int>
+    // store         = 'S'<int>','<int>
+    // storeI32      = 'Si'<int>','<int>
+    // storeDbl      = 'Si'<int>','<int>
+    // load          = 'L'<int>','<int>
+    // loadI32       = 'Li'<int>','<int>
+    // loadDbl       = 'Ld'<int>','<int>
+    // eqI32         = '=i'<int>','<int>
+    // const         = 'C'<int>','<datum>
+    // extractI32    = 'Ei'<int>','<int>
+    // extractDbl    = 'Ed'<int>','<int>
     // jump          = 'J'<int>
-    // if            = 'I'<int>
-    // if equal ints = 'I=i'<int>
-    // equal ints    = '=i'
-    // increment int = '++i'<int>
-    // add doubles   = '+d'
-    // call          = 'C'<int>
-    // execute       = 'E'
-    // exit          = 'X'
-    // datum         = 'd'<double> | 'i'<int> | 'e'<external function name>
-    // resize        = 'V'<int>
+    // ifI32         = 'Ii'<int>','<int>
+    // addI32        = '+i'<int>','<int>','<int>
+    // call          = '()'<int>','<name>(','<int>)*'
+    // return        = 'R'<int>
+    // datum         = 'd'<double> | 'i'<int>
     //
     // Example:
-    //     "Pd2|Pd3|+d|X"
-    // Means to push 2.0, push 3.0, add the values, then return the result.
+    //     "C8,d3|()9,foo,1,2,8|R9"
+    // Means:
+    // 1. Assign the datum 3.0 to variable 8.
+    // 2. Call the function 'foo' with the values in 1, 2, and 8; and assign
+    //    the result to variable 9.
+    // 3. Return the value in variable 9.
     //
     // Note that more capabilities will be added as needed.
     //
@@ -68,9 +82,7 @@ struct BytecodeDSLUtil {
     typedef BloombergLP::bslma::Allocator Allocator;   // for convenience
     typedef BloombergLP::bslstl::StringRef StringRef;  // for convenience
 
-    typedef bsl::unordered_map<bsl::string,
-                               sjtd::DatumUdtUtil::ExternalFunction>
-            FunctionNameToAddressMap;
+    typedef bsl::unordered_map<bsl::string, sjtt::Function> FunctionMap;
         // Describes a map used to associated the names of functions in the DSL
         // with the addresses of actual functions.
 
@@ -78,27 +90,24 @@ struct BytecodeDSLUtil {
     static int readDatum(Datum                           *result,
                          bsl::string                     *errorMessage,
                          Allocator                       *allocator,
-                         const StringRef&                 source,
-                         const FunctionNameToAddressMap&  functions);
+                         const StringRef&                 source);
         // Read, into the specified 'result', the datum described by the
         // specified 'source', using the specified 'allocator' to allocate
-        // memory and the specified 'functions' to translate function names
-        // into addresses and return 0.  If 'source' does not encode a valid
-        // datum value (as described above), load, into the specified
-        // 'errorMessage', a string describing the problem and return a
-        // non-zero value.
+        // memory.  If 'source' does not encode a valid datum value (as
+        // described above), load, into the specified 'errorMessage', a string
+        // describing the problem and return a non-zero value.
 
-    static int readDSL(bsl::vector<sjtt::Bytecode>     *result,
-                       bsl::string                     *errorMessage,
-                       const StringRef&                 dsl,
-                       const FunctionNameToAddressMap&  functions);
+    static int readDSL(bsl::vector<sjtt::Bytecode> *result,
+                       bsl::string                 *errorMessage,
+                       const StringRef&             dsl,
+                       const FunctionMap&           functions);
         // Load, into the specified 'result', the bytecodes described in the
         // specified 'dsl' and return 0 if 'dsl' is valid; otherwise return a
         // non-zero value and load a description into the specified
-        // 'errorMessage'.  Translate function names to addresses using the
-        // specified 'functions' map.  Note that the state of 'result' after a
-        // failed parse is undefined.  Note also that the allocator associated
-        // with 'result' is used to allocate memory for any returned 'Datum'
+        // 'errorMessage'.  Translate function names using the specified
+        // 'functions' map.  Note that the state of 'result' after a failed
+        // parse is undefined.  Note also that the allocator associated with
+        // 'result' is used to allocate memory for any returned 'Datum'
         // objects.
 };
 }
